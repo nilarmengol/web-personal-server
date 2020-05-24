704cad892546d464d61572704af9ffef88910330
Nil <nilarmengol@gmail.com>
2020-05-24 15:17:12 +0200

    signup controller

--- controllers/user.js
+++ controllers/user.js
@@ -2,7 +2,41 @@ const bcrypt = require("bcrypt-nodejs");
 const User = require("../models/user");
 
 function signUp(req, res) {
-  console.log("endpoint de signup");
+  const user = new User();
+
+  const { name, lastname, email, password, repeatPassword } = req.body;
+  user.name = name;
+  user.lastname = lastname;
+  user.email = email;
+  user.role = "admin";
+  user.active = false;
+
+  if (!password || !repeatPassword) {
+    res.status(404).send({ message: "Las contraseñas son obligatorias" });
+  } else {
+    if (password != repeatPassword) {
+      res.status(404).send({ message: "Las contraseñas no son iguales" });
+    } else {
+      bcrypt.hash(password, null, null, function (err, hash) {
+        if (err) {
+          res.status(500).send({ message: "Error al encriptar la contraseña" });
+        } else {
+          user.password = hash;
+          user.save((err, userStored) => {
+            if (err) {
+              res.status(500).send({ message: "EL usuario ya existe" });
+            } else {
+              if (!userStored) {
+                res.status(500).send({ message: "Error al crear el usuario." });
+              } else {
+                res.status(200).send({ user: userStored });
+              }
+            }
+          });
+        }
+      });
+    }
+  }
 }
 
 module.exports = {
