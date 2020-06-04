1650bb558c71234f148d494b65d1730397aa3006
Nil <nilarmengol@gmail.com>
2020-06-04 12:00:28 +0200

    des/activate user

--- routers/user.js
+++ routers/user.js
@@ -18,5 +18,10 @@ api.put(
 );
 api.get("/get-avatar/:avatarName", UserController.getAvatar);
 api.put("/update-user/:id", [md_auth.ensureAuth], UserController.updateUser);
+api.put(
+  "/activate-user/:id",
+  [md_auth.ensureAuth],
+  UserController.activateUser
+);
 
 module.exports = api;
